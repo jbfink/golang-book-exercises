75cc659c98f41fcf6ca942325ba946cf37d11f3d
jbfink <john.fink@gmail.com>
2016-12-11 23:01:40 -0500

    chapter 4 end

--- chapter4/main.go
+++ chapter4/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var x string
-	x = "first "
-	fmt.Println(x)
-	x = x + "second"
-	fmt.Println(x)
+	fmt.Print("Enter a number: ")
+	var input float64
+	fmt.Scanf("%f", &input)
+
+	output := input * 2
+
+	fmt.Println(output)
 }
