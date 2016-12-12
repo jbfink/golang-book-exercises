defdd46f2b51853017760064f2d5099f66041f0d
jbfink <john.fink@gmail.com>
2016-12-12 13:07:54 -0500

    chapter 5: even/odd

--- chapter5/main.go
+++ chapter5/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	i := 1
-	for i <= 10 {
-		fmt.Println(i)
-		i = i + 1
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "even")
+		} else {
+			fmt.Println(i, "odd")
+		}
 	}
 }
