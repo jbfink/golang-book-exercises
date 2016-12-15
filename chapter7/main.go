6d0bd04419653eb7d924d855bf840193db5e3aaa
jbfink <john.fink@gmail.com>
2016-12-14 21:49:13 -0500

    chapter 7: average in function, rewritten main function

--- chapter7/main.go
+++ chapter7/main.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func average(xs []float64) float64 {
-	panic("Not Implemented")
+	total := 0.0
+	for _, v := range xs {
+		total += v
+	}
+	return total / float64(len(xs))
 }
 
 func main() {
 	xs := []float64{98, 93, 77, 82, 83}
 
-	total := 0.0
-	for _, v := range xs {
-		total += v
-	}
-	fmt.Println(total / float64(len(xs)))
+	fmt.Println(average(xs))
 }
